4bd4d070abd01ec9884106287179a20685689aba
Liz Abinante <eabinante@gmail.com>
2017-02-08 21:12:01 -0800

    :dash: start by gathering missing solutions for cells

--- app.rb
+++ app.rb
@@ -1,86 +1,9 @@
 # No Gemfile here sorry I ain't sorry
 # (I have these installed for my Ruby version)
 require 'pry'
-require 'pry-nav'
 
-# Represents a single cell in the Sudoku puzzle.
-# Coordinates are offered by @row and @column (1-9)
-# A cell is considered solved when the @solution != 0
-# Cells are required to have coordinates.
-# Cell solution defaults to 0 and must be set manually for solved cells.
-class Cell
-  attr_reader :row, :column, :solution, :possible_solutions
-
-  def initialize(row, column, solution = 0)
-    @row = row
-    @column = column
-    @solution = solution
-    @possible_solutions = []
-  end
-
-  def solve
-    if @possible_solutions.length == 1
-      @solution = @possible_solutions.pop
-    end
-  end
-
-  def solved?
-    !@solution.zero?
-  end
-
-  def coordinates
-    [@row, @column]
-  end
-
-  def to_s
-    "row: #{@row}, column: #{@column}"
-  end
-end
-
-# Parent class for groups of cells. Used by Row, Column, and Grid.
-# Provides shared methods for checking and validating solutions.
-class CellGroup
-  attr_reader :cells
-
-  def solved?
-    @cells.length == 9 && @cells.all? { |cell| cell.solved? } && valid_solution
-  end
-
-  def solutions
-    @cells.map { |cell| cell.solution }
-  end
-
-  protected
-  def valid_solution
-    solutions.uniq.sort == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  end
-end
-
-# Collection of 9 cells that belong in a grid.
-# Grids are numbered from top to right, with the top left being 1
-# and the bottom right being 9.
-class Grid < CellGroup
-  attr_reader :position
-
-  def initialize(cells, position)
-    @cells = cells
-    @position = position
-  end
-end
-
-# Collection of cells that belong in the same column row.
-class Row < CellGroup
-  def initialize(cells)
-    @cells = cells.sort_by(&:column)
-  end
-end
-
-# Collection of cells that belong in the same column.
-class Column < CellGroup
-  def initialize(cells)
-    @cells = cells.sort_by(&:row)
-  end
-end
+# all the code for cells and cell groups
+require_relative 'cells'
 
 # Create and solve and Sudoku puzzle!
 class Sudoku
@@ -91,8 +14,43 @@ class Sudoku
     build
   end
 
+  # WIP! get the logic, clean it up :)
   def solve
-    # solve the puzzle!
+    unsolved_cells = @cells.select { |cell| !cell.solved? }
+    unsolved_cells.each do |cell|
+      column = column(cell.column)
+      row = row(cell.row)
+      grid = grid(cell)
+
+      missing_solutions = {}
+      missing_solutions[:column] = column.missing_solutions
+      missing_solutions[:row] = row.missing_solutions
+      missing_solutions[:grid] = grid.missing_solutions
+
+      solutions = {}
+      solutions[:column] = column.solutions
+      solutions[:row] = row.solutions
+      solutions[:grid] = grid.solutions
+
+      groups = [:column, :row, :grid]
+      missing_solutions.each do |key, missing|
+        check_list = groups
+        check_list.delete(key)
+        check_list.each do |group|
+          missing.delete_if { |invalid| solutions[group].include?(invalid) }
+          cell.possible_solutions << missing
+        end
+      end
+
+      cell.possible_solutions.flatten!.uniq!.sort!
+
+      # for debugging!
+      puts cell.possible_solutions.join(" - ")
+      cell.solve
+
+      # for debugging!
+      puts "solved a cell!" if cell.solved?
+    end
   end
 
   def solved?
@@ -100,11 +58,15 @@ class Sudoku
   end
 
   def row(number)
-    @rows[number+=1]
+    @rows[number-=1]
   end
 
   def column(number)
-    @columns[number+=1]
+    @columns[number-=1]
+  end
+
+  def grid(cell)
+    @grids.select { |grid| grid.cells.include?(cell) }.first
   end
 
   protected
@@ -128,6 +90,7 @@ class Sudoku
   end
 
   def build_grids
+    @grids = []
     row_starts = [1, 4, 7]
     grid_maps = []
 
@@ -144,6 +107,6 @@ class Sudoku
       end
     end
 
-    @grids = grid_maps.each_with_index { |grid, index| Grid.new(grid, index+1) }
+    grid_maps.each_with_index { |grid, i| grids << Grid.new(grid, i+1) }
   end
 end
